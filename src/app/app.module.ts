54ffac273cb6e040959b941e94d159dc292fbf1b
AngelQuesada <angelquesadaruiz@gmail.com>
2019-11-20 13:46:50 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// Display component
+
+import { HttpClientModule } from '@angular/common/http'
+import { WeatherDataService } from './services/weatherdata.service';
+import { DisplayComponent } from './components/display/display.component';
+
+// Selection Component
+
+import { SelectionComponent } from './components/selection/selection.component'
+import { FormsModule } from '@angular/forms'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    SelectionComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [WeatherDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/weatherdata.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+let serviceUrl: String = 'https://api.openweathermap.org/data/2.5/weather'
+let apiKey: String = 'a05f4cc77a91b03c44d8233d0bb69d73'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WeatherDataService {
+  constructor(private http: HttpClient) { }
+  load(city: String) {
+    return this.http.get(serviceUrl + '?q=' + city + '&APPID=' + apiKey)
+  }
+  getIconUrl(icon: String) {
+    return 'http://openweathermap.org/img/w/' + icon + ".png"
+  }
+}
